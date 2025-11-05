b32d4eb6f56145035a9b6379826698cc169fd869
Andrea Duarte <ajduarteg@uamv.edu.ni>
2025-11-05 16:10:44 -0600

    changes usuario

--- src/main/java/entities/Usuario.java
+++ src/main/java/entities/Usuario.java
@@ -6,6 +6,7 @@ import java.util.List;
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,10 +18,45 @@ public class Usuario {
     @OneToMany(mappedBy = "usuario")
     private List<Tarea> tareas;
 
-    public Usuario() {
-    }
+    public Usuario() {}
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(List<Tarea> tareas) {
+        this.tareas = tareas;
     }
 }
